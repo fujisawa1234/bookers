d5051f03604f8a699891263b092e0468f5e63566
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-09-11 07:25:50 +0000

    [fix]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
- 
+
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to book_path(book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -20,10 +22,10 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find (params[:id])
   end
-  
-  
+
+
   private
-  def book_params
+    def book_params
     params.require(:book).permit(:title, :body)
-  end
+    end
 end
